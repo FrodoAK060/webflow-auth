9dfa1b46a5fd662d2e733a0c30e6e02d73f71806
Fyodor <chasolist@gmail.com>
2025-11-24 06:24:13 +0500

    Implement login function

--- src/auth.js
+++ src/auth.js
@@ -8,3 +8,10 @@ module.exports = { login };
 function checkCredentials(username, password) {
     return username && password;
 }
+
+function login(username, password) {
+    if (checkCredentals(username, password)) {
+        return { success: true, token: 'abc123' };
+    }
+    return { success: false };
+}
